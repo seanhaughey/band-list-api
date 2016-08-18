97cf3a3bb866dcfb61fda62bcd25c852087c47f7
Sean Haughey <sdhaughey@gmail.com>
2016-08-18 16:48:20 -0500

    initial commit

--- models/band.js
+++ models/band.js
@@ -1,2 +1,13 @@
 // TODO: Add and export band model
 // attrs: name, genre, corruptedByTheSystem
+const mongoose = require('mongoose');
+
+const bandSchema = {
+  name: String,
+  genre: String,
+  corruptedByTheSystem: {type: Boolean, default: false}
+}
+
+const Band = mongoose.model('Band', bandSchema);
+
+module.exports = Band;
--- routes/bands.js
+++ routes/bands.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const Band = require('../models/band')
+const _ = require('lodash');
 
+router.use(function (req, res, next) {
+  req.body = _.pick(req.body, ['name', 'genre', 'corruptedByTheSystem'])
+  next();
+})
 
 
 module.exports = router;
